a7388b2c044313f319c2ecebea66230af541b748
psmyrdek <przemek.smyrdek@gmail.com>
2024-11-27 20:03:45 +0100

    chore: description

--- tasks/core-pro/003-generics-mergable/task.ts
+++ tasks/core-pro/003-generics-mergable/task.ts
@@ -1,6 +1,4 @@
-// type MergeableObject<T> = T extends object ? (T extends any[] | Function ? never : T) : never;
-
-// Zaimplementuj typ MergeableObject z wykorzystaniem typów Exclude i NonNullable.
+// Zaimplementuj typ MergeableObject z wykorzystaniem typów wbudowanych - Exclude i NonNullable.
 type MergeableObject<T> = T;
 
 export function mergeObjects<T, U>(obj1: MergeableObject<T>, obj2: MergeableObject<U>): T & U {
