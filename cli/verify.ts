a71ea7e29db2cf589a46bddff9159e1a7a677f7d
“mkczarkowski” <marcinczarkowski@onet.eu>
2024-11-30 12:51:34 +0100

    feat: allow searching for a task without numerals

--- cli/verify.ts
+++ cli/verify.ts
@@ -1,6 +1,6 @@
+import { Command } from 'commander';
 import { glob } from 'glob';
 import { startTest } from './scripts/test-runner.ts';
-import { Command } from 'commander';
 
 const program = new Command();
 
@@ -12,13 +12,19 @@ program
   .action(async (task, options: { watch: boolean }) => {
     try {
       const paths = await glob(`tasks/**/${task}`);
+      const taskWithoutNumber = task.replace(/^\d+-/, '');
+      const alternativePaths = await glob(`tasks/**/*-${taskWithoutNumber}`);
 
-      if (paths.length === 0) {
-        console.error(`👉 Upewnij się, że zadanie o nazwie "${task}" istnieje`);
+      const allPaths = [...new Set([...paths, ...alternativePaths])];
+
+      if (allPaths.length === 0) {
+        console.error(
+          `👉 Upewnij się, że zadanie o nazwie "${task}" lub "*-${taskWithoutNumber}" istnieje`,
+        );
         process.exit(1);
       }
 
-      await startTest(`${paths[0]}`, { watch: options.watch });
+      await startTest(`${allPaths[0]}`, { watch: options.watch });
     } catch (error) {
       console.error(`\n❌ Nieoczekiwany błąd :(\n\n ${error}`);
       process.exit(1);
