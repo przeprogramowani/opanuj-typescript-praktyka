314e57d43b9b64aabe9cafd1d575c7edeb574003
“mkczarkowski” <marcinczarkowski@onet.eu>
2024-12-01 12:33:32 +0100

    feat: add global declaration task

---
+++ tasks/core-pro/006-extended-window/task.ts
@@ -0,0 +1,5 @@
+/* Rozszerz interfejs globalnego obiektu window o obiekt analytics z metodę trackEvent(string): void.
+  Zrób to w sposób globalny, który będzie dostępny w całym projekcie.
+*/
+
+window.analytics.trackEvent('click');
