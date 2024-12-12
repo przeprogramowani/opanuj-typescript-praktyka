d3b8b303df5d33564278522dcef6d3098e9492c5
psmyrdek <przemek.smyrdek@gmail.com>
2024-12-12 21:10:07 +0100

    fix: text

--- tasks/react-pro/511-intercept-it/intercept.spec.tsx
+++ tasks/react-pro/511-intercept-it/intercept.spec.tsx
@@ -1,9 +1,8 @@
-import { act } from 'react';
 import { join } from 'path';
 import { describe, test } from 'vitest';
 import { getCompilerDiagnostics } from '../../../utils/ts-utils';
 import App from './App';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { setupMockServer } from '../../../utils/vitest/msw';
 
@@ -18,17 +17,16 @@ describe('Axios Interceptor', () => {
   test('does not call tracker API when request is immediate', async () => {
     render(<App />);
     const searchButton = screen.getByTestId('search-button-new');
-    await act(async () => {
-      await userEvent.click(searchButton);
-    });
+    await userEvent.click(searchButton);
+
     expect(verifyRequest('http://localhost:3000/api/tracker', 'POST')).toBe(false);
   });
 
   test('calls tracker API when request takes 5 seconds', async () => {
     render(<App />);
     const searchButton = screen.getByTestId('search-button-legacy');
+    await userEvent.click(searchButton);
     await act(async () => {
-      await userEvent.click(searchButton);
       await vi.advanceTimersByTimeAsync(5000);
     });
 
