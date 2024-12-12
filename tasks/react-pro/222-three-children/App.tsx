5b05c39092d95ac52c8a669f2dd1b456fd01fec1
“mkczarkowski” <marcinczarkowski@onet.eu>
2024-12-12 21:44:22 +0100

    feat: add task

---
+++ tasks/react-pro/222-three-children/App.tsx
@@ -0,0 +1,15 @@
+/*
+  Dodaj typowanie propsa children dla TripleContainer, które wymusi przekazanie dokładnie trzech komponentów lub elementów HTML.
+*/
+
+const TripleContainer = ({ children }) => <ul className="list-disc">{children}</ul>;
+
+const App = () => (
+  <TripleContainer>
+    <li>First</li>
+    <li>Second</li>
+    <li>Third</li>
+  </TripleContainer>
+);
+
+export { App, TripleContainer };
