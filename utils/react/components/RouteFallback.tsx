81fa3b6dacb22afc1eba27466f2201127bdee9c4
psmyrdek <przemek.smyrdek@gmail.com>
2024-12-13 17:49:13 +0100

    feat: boundary

---
+++ utils/react/components/RouteFallback.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router';
+import { Wrench } from 'lucide-react';
+interface RouteFallbackProps {
+  exerciseName: string;
+}
+
+export default function RouteFallback({ exerciseName }: RouteFallbackProps) {
+  return (
+    <div>
+      <h2 className="flex items-center gap-2 text-lg font-bold">
+        <Wrench className="w-4 h-4" /> Projekt "{exerciseName}" nie może zostać uruchomiony.
+      </h2>
+      <p className="mb-8">Upewnij się, że implementacja jest poprawna, a kod się kompiluje.</p>
+      <Link className="text-blue-400" to="/">
+        Wróć do strony głównej
+      </Link>
+    </div>
+  );
+}
