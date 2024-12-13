479d79bcbe448479fee5cdd9426b8b1ec9071b80
psmyrdek <przemek.smyrdek@gmail.com>
2024-12-13 17:26:35 +0100

    chore: grid instead of flex

--- utils/react/RootApp.tsx
+++ utils/react/RootApp.tsx
@@ -7,14 +7,14 @@ type RootAppProps = {
 const RootApp = ({ routes }: RootAppProps) => {
   return (
     <nav>
-      <ul className="flex flex-wrap gap-4 text-center">
-        {routes.map(({ exerciseName, path }) => (
-          <li key={exerciseName} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {routes.map(({ exerciseName }) => (
+          <li key={exerciseName} className="w-full text-center">
             <NavLink
               to={`/${exerciseName}`}
               className="block p-6 bg-gray-800 rounded-lg shadow-lg transition-transform transform hover:-translate-y-2 hover:shadow-xl card-hover"
             >
-              <h2 className="text-xl font-semibold text-blue-300">{exerciseName}</h2>
+              <h2 className="text-md lg:text-xl font-semibold text-blue-300">{exerciseName}</h2>
             </NavLink>
           </li>
         ))}
